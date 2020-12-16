0b0b88d58f4fac9886c44a63c96dfe8a04cf029a
danilochf <danilochf@yahoo.com.br>
2020-12-16 10:59:58 -0300

    Criação Departamentos

---
+++ VendasWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> lista = new List<Department>();
+            lista.Add(new Department { Id = 1, Nome = "Eletrônicos" });
+            lista.Add(new Department { Id = 2, Nome = "Móveis e Decoração" });
+
+            return View(lista);
+        }
+    }
+}
