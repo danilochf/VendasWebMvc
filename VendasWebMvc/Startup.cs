36602859b04f27eacaa8232bfcd647d1f1661e32
danilochf <danilochf@yahoo.com.br>
2021-01-20 14:06:15 -0300

    Formatações

--- VendasWebMvc/Startup.cs
+++ VendasWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using VendasWebMvc.Models;
 using VendasWebMvc.Data;
@@ -51,6 +53,15 @@ namespace VendasWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localzationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localzationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
